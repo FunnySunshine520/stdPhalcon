3a66eda9730368ff0211b3b43a418dd655d97e0a
14254 <1425407707@qq.com>
2018-08-01 10:52:38 +0800

    add IndexController.php

---
+++ apps/frontend/controllers/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 14254
+ * Date: 2018/8/1
+ * Time: 10:49
+ */
+use Phalcon\Mvc\Controller;
+
+class IndexController extends Controller
+{
+    public function indexAction() {
+        echo "This is indexAction";
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 14254
+ * Date: 2018/8/1
+ * Time: 10:12
+ */
+echo phpinfo();
+echo "hello";
